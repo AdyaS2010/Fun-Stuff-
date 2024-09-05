40d06afa23615cd61532a19f5a30ae5357d3b668
AdyaS2010 <adya.sastry@gmail.com>
2024-09-04 21:01:16 -0500

    Update adventure_game.py

--- adventure_game.py
+++ adventure_game.py
@@ -1,37 +1,6 @@
-def main():
-    inventory = []
-
-    while True:
-        print("\nYou find yourself in a dark forest. What do you want to do?")
-        print("1. Look around")
-        print("2. Walk forward")
-        print("3. Check inventory")
-        print("4. Quit")
-
-        choice = input("Enter your choice: ")
-
-        if choice == "1":
-            print("You see trees all around you.")
-        elif choice == "2":
-            print("You walk forward and find a path.")
-        elif choice == "3":
-            if inventory:
-                print("Your inventory contains:", ", ".join(inventory))
-            else:
-                print("Your inventory is empty.")
-        elif choice == "4":
-            print("Thanks for playing!")
-            break
-        else:
-            print("Invalid choice. Please try again.")
-
-if __name__ == "__main__":
-    main()
-
-""" Other direction I might go in, similar to bag in a way, more interactive and user-controlled:  
 def show_instructions():
     print("Welcome to the Adventure Game!")
-    print("Commands: go [direction], get [item]")
+    print("Commands: look, go [direction], get [item], inventory, quit")
 
 def show_status():
     print("---------------------------")
@@ -43,28 +12,47 @@ def show_status():
 
 inventory = []
 rooms = {
-    'Hall': {'south': 'Kitchen', 'east': 'Dining Room', 'item': 'key'},
+    'Forest': {'north': 'Hall', 'item': 'map'},
+    'Hall': {'south': 'Forest', 'east': 'Dining Room', 'item': 'key'},
     'Kitchen': {'north': 'Hall', 'item': 'monster'},
     'Dining Room': {'west': 'Hall', 'south': 'Garden'},
     'Garden': {'north': 'Dining Room'}
 }
 
-current_room = 'Hall'
+current_room = 'Forest'
 
 show_instructions()
 
 while True:
     show_status()
     move = input(">").lower().split()
-    if move[0] == 'go':
+    
+    if move[0] == 'look':
+        print("You see trees all around you." if current_room == 'Forest' else "You see a " + rooms[current_room].get('item', 'nothing special') + " here.")
+    
+    elif move[0] == 'go':
         if move[1] in rooms[current_room]:
             current_room = rooms[current_room][move[1]]
         else:
             print("You can't go that way!")
-    if move[0] == 'get':
+    
+    elif move[0] == 'get':
         if 'item' in rooms[current_room] and move[1] == rooms[current_room]['item']:
             inventory.append(move[1])
             print(move[1] + " got!")
             del rooms[current_room]['item']
         else:
             print("Can't get " + move[1] + "!")
+    
+    elif move[0] == 'inventory':
+        if inventory:
+            print("Your inventory contains:", ", ".join(inventory))
+        else:
+            print("Your inventory is empty.")
+    
+    elif move[0] == 'quit':
+        print("Thanks for playing!")
+        break
+    
+    else:
+        print("Invalid command. Please try again.")
